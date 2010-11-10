0d6808e7bc271f2298ef67509851da987922da75
Felix Co <ichentwickle@arcor.de>
2010-11-10 22:40:20 +0100

    fixed permission problem with gpgmail

--- payload/gpgmail-postinstall.sh
+++ payload/gpgmail-postinstall.sh
@@ -4,6 +4,8 @@ tempdir=/private/tmp/GPGMail_Installation
 mkdir -p "$HOME/Library/Mail/Bundles"
 # The installer has to make sure, that the "GPGMail.mailbundle" is installed in $tempdir
 mv "$tempdir/GPGMail.mailbundle" "$HOME/Library/Mail/Bundles/"
+# change the user and group to avoid problems when updating (so this skript needs to be run as root!)
+chown -R $USER:Staff "$HOME/Library/Mail/Bundles/GPGMail.mailbundle"
 
 # cleanup tempdir "rm -d" deletes the temporary installation dir only if empty.
 # that is correct because if eg. /tmp is you install dir, there can be other stuff
