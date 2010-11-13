77ee23560c74c4bfeb0b94c9127807e1278a5914
Felix Co <ichentwickle@arcor.de>
2010-11-13 09:49:58 +0100

    added script to create DMG, fixed name of uninstall script

---
+++ create_dmg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 
+# This script creates a DMG  for GPGTools
+#
+# (c) by Felix Co
+#
+
+tar xfvj template.dmg.tar.bz2
+
+hdiutil attach "template.dmg" -noautoopen -quiet -mountpoint "gpgtools_diskimage"
+
+
+# Copy the relevant files
+ditto --rsrc build/GPGTools.mpkg gpgtools_diskimage/GPGTools.mpkg
+ditto --rsrc Uninstall_GPGTools.app gpgtools_diskimage/Uninstall_GPGTools.app
+
+# get the name of the dvice to detatch it
+dmg_device=` hdiutil info | grep "gpgtools_diskimage" | awk '{print $1}' `
+
+hdiutil detach $dmg_device -quiet -force
+
+hdiutil convert "template.dmg" -quiet -format UDZO -imagekey zlib-level=9 -o "build/GPGTools-$(date "+%Y%m%d").dmg"
+
+zip build/GPGTools-$(date "+%Y%m%d").dmg.zip build/GPGTools-$(date "+%Y%m%d").dmg
+
+# remove the extracted template
+rm template.dmg
