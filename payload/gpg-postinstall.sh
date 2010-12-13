91a278d15d209e83ddfe752a1f7d280819f439e6
Felix Co <ichentwickle@arcor.de>
2010-12-13 14:29:18 +0100

    installer now chreates a gpg.conf if none exists [#8 state:resolved]

---
+++ payload/gpg-postinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Create a new gpg.conf if none is existing from the skeleton file
+if ( ! test -e $HOME/.gnupg/gpg.conf ) then
+	echo "Create!"
+	cp /usr/local/share/gnupg/options.skel $HOME/.gnupg/gpg.conf
+fi
