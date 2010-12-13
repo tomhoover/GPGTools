64d6cb63487b826ada65d11c9a3ede63d2b4e078
Felix Co <ichentwickle@arcor.de>
2010-12-13 06:27:49 +0100

    added patched gpg-agent, agent now gets restarted after installation [#6 state:resolved]

--- payload/gpgagent-postinstall.sh
+++ payload/gpgagent-postinstall.sh
@@ -2,3 +2,8 @@
 
 # Add the gpg-agent helper Script to login items for the current user:
 osascript -e 'tell application "System Events" to make new login item at end with properties {path:"/usr/local/libexec/start-gpg-agent.app", hidden:true}' > /dev/null
+
+# Stop the agent
+killall gpg-agent
+# Start the agent under the logged in user
+sudo -u $USER /usr/local/libexec/start-gpg-agent.app/Contents/MacOS/start-gpg-agent
