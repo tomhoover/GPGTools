26cdce3d2a8931949c088755f7ceed433de3280e
Felix Co <ichentwickle@arcor.de>
2010-12-20 15:50:12 +0100

    added check if Thunderbird is installed

---
+++ payload/check_for_thunderbird.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+thunderbirdextensiondir=$HOME/Library/Thunderbird/Profiles/$(ls $HOME/Library/Thunderbird/Profiles | grep default)/extensions
+
+if ( test -e $thunderbirdextensiondir ) then
+	exit 0
+fi
+
+exit 1
