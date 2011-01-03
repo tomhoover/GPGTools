53d5d7f88ce1c3e42f8ad16a895ca00b82169c4f
Alexander Willner <alex@willner.ws>
2011-01-03 07:37:14 +0100

    fixed #25 state:resolved

--- payload/gpg-postinstall.sh
+++ payload/gpg-postinstall.sh
@@ -3,5 +3,6 @@
 # Create a new gpg.conf if none is existing from the skeleton file
 if ( ! test -e $HOME/.gnupg/gpg.conf ) then
 	echo "Create!"
+	mkdir -p $HOME/.gnupg
 	cp /usr/local/share/gnupg/options.skel $HOME/.gnupg/gpg.conf
 fi
