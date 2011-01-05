fa8284d9f658d1d2c5e9fb56bbea778b61fe51db
Alexander Willner <alex@willner.ws>
2011-01-05 07:12:57 +0100

    possible fix for [#26]

--- payload/gpg-postinstall.sh
+++ payload/gpg-postinstall.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Fix a bug with an old GPGTools installation
+	chown -R $USER:Staff $HOME/.gnupg
+
 # Create a new gpg.conf if none is existing from the skeleton file
 if ( ! test -e $HOME/.gnupg/gpg.conf ) then
 	echo "Create!"
 	mkdir -p $HOME/.gnupg
+	chown -R $USER:Staff $HOME/.gnupg
 	cp /usr/local/share/gnupg/options.skel $HOME/.gnupg/gpg.conf
 fi
