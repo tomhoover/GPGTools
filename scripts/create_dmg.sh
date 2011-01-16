066b9e4db91ac732e1cb53f0df37aeef403cdb68
Alexander Willner <alex@willner.ws>
2011-01-17 00:46:23 +0100

    added a makefile and cleaned up everything

--- scripts/create_dmg.sh
+++ scripts/create_dmg.sh
@@ -2,8 +2,13 @@
 #
 # This script creates a DMG  for GPGTools
 #
-# (c) by Felix Co
+# (c) by Felix Co & Alexander Willner
 #
+
+if ( ! test -e Makefile ) then
+	echo "Wrong directory..."
+	exit 1
+fi
 
 # Build the installer
 if ( test -e /usr/local/bin/packagesbuild ) then
@@ -19,7 +24,7 @@ fi
 rm build/GPGTools-$(date "+%Y%m%d").dmg
 #rm build/GPGTools-$(date "+%Y%m%d").dmg.zip
 
-tar xfvj template.dmg.tar.bz2
+tar xfvj resources/template.dmg.tar.bz2
 
 hdiutil attach "template.dmg" -noautoopen -quiet -mountpoint "gpgtools_diskimage"
 
@@ -27,10 +32,10 @@ hdiutil attach "template.dmg" -noautoopen -quiet -mountpoint "gpgtools_diskimage
 # Copy the relevant files
 ditto --rsrc build/GPGTools.mpkg gpgtools_diskimage/Install\ GPGTools.mpkg
 ditto --rsrc Uninstall_GPGTools.app gpgtools_diskimage/Uninstall\ GPGTools.app
-cp gpgtoolsdmg.icns gpgtools_diskimage/.VolumeIcon.icns
-cp dmg_background.png gpgtools_diskimage/.background/dmg_background.png
-./setfileicon trash.icns gpgtools_diskimage/Uninstall\ GPGTools.app
-./setfileicon installer.icns gpgtools_diskimage/Install\ GPGTools.mpkg
+cp images/gpgtoolsdmg.icns gpgtools_diskimage/.VolumeIcon.icns
+cp images/dmg_background.png gpgtools_diskimage/.background/dmg_background.png
+./scripts/setfileicon images/trash.icns gpgtools_diskimage/Uninstall\ GPGTools.app
+./scripts/setfileicon images/installer.icns gpgtools_diskimage/Install\ GPGTools.mpkg
 
 # get the name of the dvice to detatch it
 dmg_device=` hdiutil info | grep "gpgtools_diskimage" | awk '{print $1}' `
@@ -39,7 +44,7 @@ hdiutil detach $dmg_device -quiet -force
 
 hdiutil convert "template.dmg" -quiet -format UDZO -imagekey zlib-level=9 -o "build/GPGTools-$(date "+%Y%m%d").dmg"
 
-#zip -j build/GPGTools-$(date "+%Y%m%d").dmg.zip build/GPGTools-$(date "+%Y%m%d").dmg
-
 # remove the extracted template
 rm template.dmg
+
+open build/GPGTools-$(date "+%Y%m%d").dmg
