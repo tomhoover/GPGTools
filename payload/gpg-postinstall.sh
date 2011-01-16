5aee551cda8ad6f108ff251869977047921a5276
Alexander Willner <alex@willner.ws>
2011-01-16 22:42:57 +0100

    better cleanup

--- payload/gpg-postinstall.sh
+++ payload/gpg-postinstall.sh
@@ -3,6 +3,10 @@
 # Fix a bug with an old GPGTools installation
 	chown -R $USER:Staff $HOME/.gnupg
 
+# Also clean up bad GPGTools behaviour:
+  [ -h "$HOME/.gnupg/S.gpg-agent" ] && rm -f "$HOME/.gnupg/S.gpg-agent"
+  [ -h "$HOME/.gnupg/S.gpg-agent.ssh" ] && rm -f "$HOME/.gnupg/S.gpg-agent.ssh"
+
 # Create a new gpg.conf if none is existing from the skeleton file
 if ( ! test -e $HOME/.gnupg/gpg.conf ) then
 	echo "Create!"
