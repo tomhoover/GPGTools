ecda5d3d5639bc9aa4cd9e26e8ba76b4550956cd
Alexander Willner <alex@willner.ws>
2011-01-25 01:24:01 +0100

    moved GPGServices to HOME

--- payload/gpgservices-postinstall.sh
+++ payload/gpgservices-postinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+mkdir -p $HOME/Library/Services/
+mv /Users/Shared/GPGServices.service $HOME/Library/Services/
+
+mkdir -p $HOME/Library/PreferencePanes/
+mv /Users/Shared/GPGTools.prefPane $HOME/Library/PreferencePanes/
+
 ./ServicesRestart
 sleep 3
 sudo ./ServicesRestart
---
+++ payload/gpgtoolspreferences-postflight.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mv /Users/Shared/GPGTools.prefPane ~/Library/PreferencePanes/
+
+exit 0;
