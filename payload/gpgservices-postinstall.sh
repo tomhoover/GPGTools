71f8f203251bc9ba5197f3e0490647e16cae0cdb
Alexander Willner <alex@willner.ws>
2011-05-16 16:40:01 +0200

    cleanup

--- payload/gpgservices-postinstall.sh
+++ payload/gpgservices-postinstall.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Old version must not run in the background
 killall GPGServices
 sleep 1
 killall -9 GPGServices
 
+# Remove (old) versions
 rm -rf $HOME/Library/Services/GPGServices.service
 rm -rf /Library/Services/GPGServices.service
 
