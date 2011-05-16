1a8d802894291f01e3f94b9d9ac488de59e52fee
Alexander Willner <alex@willner.ws>
2011-05-16 16:51:37 +0200

    also install to the HOME

--- payload/gpgservices-postinstall.sh
+++ payload/gpgservices-postinstall.sh
@@ -5,18 +5,29 @@ killall GPGServices
 sleep 1
 killall -9 GPGServices
 
+# Where to install it
+_target="/Library/Services/"
+if [ -e "$HOME/Library/Services/GPGServices.service" ]; then
+    _target="$HOME/Library/Services/"
+    chown -R $USER "$_target"
+fi
+
 # Remove (old) versions
 rm -rf $HOME/Library/Services/GPGServices.service
 rm -rf /Library/Services/GPGServices.service
 
-mkdir -p /Library/Services/
-#chown -R $USER $HOME/Library/Services/
-mv /private/tmp/GPGServices.service /Library/Services/
-#chown -R $USER $HOME/Library/Services/
+# Install it
+mkdir -p "$_target"
+mv /private/tmp/GPGServices.service "$_target"
 
+# Cleanup
+if [ -e "$HOME/Library/Services/GPGServices.service" ]; then
+    chown -R $USER "$_target"
+fi
+
+# Reload keyboard preferences
 ./ServicesRestart
-sleep 3
+sleep 2
 sudo ./ServicesRestart
 
 exit 0
-
