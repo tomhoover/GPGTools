f4353e36b181c0fdc05ad46801ab7c43e30b787a
Alexander Willner <alex@willner.ws>
2011-05-19 09:12:28 +0200

    Installer now respects the old location of GPGMail [#36]

--- payload/gpgmail-preinstall.sh
+++ payload/gpgmail-preinstall.sh
@@ -3,12 +3,5 @@
 # Quit Apple Mail
 osascript -e "quit app \"Mail\""
 
-# fix permissions (http://gpgtools.lighthouseapp.com/projects/65764-gpgmail/tickets/134)
-sudo mkdir -p "$HOME/Library/Mail/Bundles"
-sudo chown -R $USER:Staff "$HOME/Library/Mail/Bundles"
-sudo chmod 755 "$HOME/Library/Mail/Bundles"
-
-# remove old version of bundle
-rm -rf "$HOME/Library/Mail/Bundles/GPGMail.mailbundle"
-# remove possible leftovers of previous installations
+# Remove possible leftovers of previous installations
 rm -rf "/tmp/GPGMail_Installation"
