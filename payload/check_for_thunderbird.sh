e1d2a397bde1d0fb992668f30cbc0ae69a27bbd6
Alexander Willner <alex@willner.ws>
2011-07-18 22:28:17 +0200

    Remove old code

--- payload/check_for_thunderbird.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-thunderbirdextensiondir=$HOME/Library/Thunderbird/Profiles/$(ls $HOME/Library/Thunderbird/Profiles | grep default)/extensions
-
-if ( test -e $thunderbirdextensiondir ) then
-	exit 0
-fi
-
-exit 1
