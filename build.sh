dff1144450565b8233d754e9e336c34000e0b129
AlexanderWillner <alexander.willner@tu-berlin.de>
2012-11-05 22:09:22 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -32,15 +32,12 @@ checkEnvironment () {
 }
 
 
-checkout () {
+checkoutProject () {
   # config
   projectName="$1"
   projectBranch="$2"
-  projectInstaller="$3"
   projectDir="${projectName}_${projectBranch}";
-  projectPath="${projectName}/"
   projectRepo="${_baseURL}${_basePath}${projectName}"
-  logFile="${projectName}.log"
 
   # checkout
   if [ ! -d "${projectDir}" ]; then 
@@ -51,38 +48,54 @@ checkout () {
   make update; [ "$?" != "0" ] && exit 1
 }
 
+createDMG () {
+    make compile; [ "$?" != "0" ] && exit 1
+    ./Dependencies/GPGTools_Core/scripts/create_dmg.sh auto buildbot; [ "$?" != "0" ] && exit 2
+}
+
 buildProject () {
   # config
   projectName="$1"
   projectBranch="$2"
-  projectInstaller="$3"
+  createDMG="$3"
   projectDir="${projectName}_${projectBranch}";
-  projectPath="${projectName}/"
   projectRepo="${_baseURL}${_basePath}${projectName}"
   logFile="${projectDir}.log"
 
-  echo " * Working on ${projectBranch} branch at ${projectRepo}..."
+  echo " * Working on ${projectBranch} branch under ${projectRepo}..."
   (
-
-  # checkout
-  checkout "$1" "$2" "$3"
-
-  if [ "${projectInstaller}" == "1" ]; then
-     make compile; [ "$?" != "0" ] && exit 1
-    ./Dependencies/GPGTools_Core/scripts/create_dmg.sh auto buildbot
-    [ "$?" != "0" ] && exit 2
-  fi
-  
+  checkoutProject "$1" "$2" "$3"
+  [ "${createDMG}" == "1" ] && createDMG
   exit 0
   ) > "${logFile}" 2>&1
   
-  if [ "$?" != "0" ]; then
-    echo "ERROR! See ${logFile}.";
-  fi
+  [ "$?" != "0" ] && echo "ERROR! See ${logFile}." && exit 2
 }
 
-cpForInstaller () {
+copyAndOverwrite () {
   rm -rf "build/payload/$2"; mkdir -p "build/payload/$2"; cp -R "../$1" "build/payload/$2"
+}
+
+copyInstallerBinaries () {
+  # todo: read makefile config and use these variables to copy the files
+  src="GPGKeychainAccess_master/build/Release/GPG Keychain Access.app"
+  dst="gka/"
+  copyAndOverwrite "${src}" "${dst}"
+  src="GPGMail_snow_leopard/build/Release/GPGMail.mailbundle"
+  dst="gpgmail106/private/tmp/GPGMail_Installation/"
+  copyAndOverwrite "${src}" "${dst}"
+  src="GPGPreferences_master/build/Release/GPGPreferences.prefPane"
+  dst="gpgtoolspreferences/"
+  copyAndOverwrite "${src}" "${dst}"
+  src="GPGMail_experimental/build/GPGMail.mpkg"
+  dst="gpgmail107/"
+  copyAndOverwrite "${src}" "${dst}"
+  src="GPGServices_master/build/GPGServices.mpkg/Contents/Packages/GPGServices.pkg"
+  dst="gpgservices"
+  copyAndOverwrite "${src}" "${dst}"
+  src="MacGPG2_homebrew/build/MacGPG2.mpkg/Contents/Packages/MacGPG2.pkg"
+  dst="MacGPG2"
+  copyAndOverwrite "${src}" "${dst}"
 }
 
 buildInstaller () {
@@ -90,28 +103,7 @@ buildInstaller () {
   cd "GPGTools_Installer_master"
   
   echo "   * Copying files..."
-  # todo: read makefile config and use these variables to copy the files
-  src="GPGKeychainAccess_master/build/Release/GPG Keychain Access.app"
-  dst="gka/"
-  cpForInstaller "${src}" "${dst}"
-  src="GPGMail_snow_leopard/build/Release/GPGMail.mailbundle"
-  dst="gpgmail106/private/tmp/GPGMail_Installation/"
-  cpForInstaller "${src}" "${dst}"
-  src="GPGPreferences_master/build/Release/GPGPreferences.prefPane"
-  dst="gpgtoolspreferences/"
-  cpForInstaller "${src}" "${dst}"
-  src="GPGMail_experimental/build/GPGMail.mpkg"
-  dst="gpgmail107/"
-  cpForInstaller "${src}" "${dst}"
-  src="GPGServices_master/build/GPGServices.mpkg/Contents/Packages/GPGServices.pkg"
-  dst="gpgservices"
-  cpForInstaller "${src}" "${dst}"
-  src="MacGPG2_homebrew/build/MacGPG2.mpkg/Contents/Packages/MacGPG2.pkg"
-  dst="MacGPG2"
-  cpForInstaller "${src}" "${dst}"
-  
-  echo "   * Creating installer..."
-  packagesbuild GPGTools.pkgproj
+  copyInstallerBinaries
 
   echo "   * Creating final DMG..."
   ./Dependencies/GPGTools_Core/scripts/create_dmg.sh auto buildbot
